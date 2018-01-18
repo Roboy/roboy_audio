d509ec72381c623fc031db9e7b6250caeb772df9
justDabuK <kevinjust87@gmail.com>
2018-01-18 14:32:58 +0100

    added doxygen like comments to describe what we have

--- roboy_audio_stream/scripts/recv_stream.py
+++ roboy_audio_stream/scripts/recv_stream.py
@@ -4,6 +4,10 @@ import rospy
 import audio_common_msgs.msg
 import message_filters
 
+"""@package roboy_audio_stream
+This module is a template for receiving the audio streams, sended by the roboy/audio_common/audio_capture module. 
+
+"""
 
 def stream_callback(data):
     """Just tells the world that it received data."""
